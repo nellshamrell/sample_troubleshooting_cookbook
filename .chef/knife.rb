77c964ba929e198d82d5282c30a5b84d196e0ea7
Nell Shamrell <nellshamrell@gmail.com>
2015-04-09 15:56:22 -0700

    sample knife config

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,6 +1,8 @@
 knife[:editor]="vim"
 
 client_key      'demo.pem'
-chef_server_url 'https://api.opscode.com/organizations/supermarket'
+#chef_server_url 'https://api.opscode.com/organizations/supermarket'
 chef_server_url 'https://192.168.33.11'
 cookbook_path   '.'
+
+knife[:supermarket_site] = 'https://192.168.33.12'
